80c6eb6dcbc5ff46708b33e391e88c9f161d198b
chen <lingd600@163.com>
2017-05-10 13:04:30 +0800

    初始化项目

--- src/com/linshi/Test.java
+++ src/com/linshi/Test.java
@@ -1,10 +1,11 @@
 package com.linshi;
-
+import
 /**
  * Created by chen on 2017/5/4.
  */
 public class Test {
     public static void main(String[] args) {
         System.out.println('1');
+
     }
 }
